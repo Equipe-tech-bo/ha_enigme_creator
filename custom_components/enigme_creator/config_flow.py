09f0e3ec6d6e20945fa5f67723a383e717984cd8
MrJulien38 <151173942+MrJulien38@users.noreply.github.com>
2026-04-10 08:57:57 +0200

    Create config_flow.py

---
+++ custom_components/enigme_creator/config_flow.py
@@ -0,0 +1,16 @@
+from homeassistant import config_entries
+from .const import DOMAIN
+
+class EnigmeCreatorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Config flow minimal pour HACS."""
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Une seule étape : juste confirmer l'installation."""
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Enigme Creator",
+                data={}
+            )
+
+        return self.async_show_form(step_id="user")
